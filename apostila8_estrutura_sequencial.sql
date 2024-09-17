335187b1ed9ebb5d7c707d01a3f5bb8f09afb535
fernando7fatec <fernando.ramos9@fatec.sp.gov.br>
2024-09-17 14:40:33 -0300

    feat: implements 1.5

--- apostila8_estrutura_sequencial.sql
+++ apostila8_estrutura_sequencial.sql
@@ -22,12 +22,15 @@ BEGIN
 END;
 $$
 
+-- **********************************************
 -- Apostila 8
--- Exercicio 1
+-- Exercicios
+-- **********************************************
 
--- Exercicio 1.1
-
--- 1.1 Faça um programa que gere um valor inteiro e o exiba.
+-- **********************************************
+-- 1.1 Faça um programa que gere 
+-- um valor inteiro e o exiba.
+-- **********************************************
 DO
 $$
 DECLARE 
@@ -37,7 +40,11 @@ BEGIN
 END;
 $$
 
--- 1.2. Faça um programa que gere um valor real e o exiba.
+
+-- **********************************************
+-- 1.2. Faça um programa que gere um valor 
+-- real e o exiba.
+-- **********************************************
 DO
 $$ 
 DECLARE
@@ -47,10 +54,14 @@ BEGIN
 END;
 $$
 
--- 1.3 Faça um programa que gere um valor real no intervalo [20, 30] 
--- que representa uma temperatura em graus Celsius. 
+
+-- **********************************************
+-- 1.3 Faça um programa que gere um valor 
+-- real no intervalo [20, 30] 
+-- que representa uma 
+-- temperatura em graus Celsius. 
 -- Faça a conversão para Fahrenheit e exiba.
--- SELECT floor(random() * (30-20+1) + 20)::int;
+-- **********************************************
 DO
 $$
 DECLARE
@@ -61,29 +72,89 @@ BEGIN
 END;
 $$
 
--- 1.4 Faça um programa que gere três valores reais a, b, e c e 
+-- **********************************************
+-- 1.4 Faça um programa que gere três 
+-- valores reais a, b, e c e 
 -- mostre o valor de delta: 
--- aquele que calculamos para chegar às potenciais raízes de uma equação do segundo grau.
-
--- formacao geral de uma eq 2o grau
--- ax + bx + c = 0
-
--- Delta
--- b**2 - 4ac
--- sendo a<>0
+-- aquele que calculamos para chegar às 
+-- potenciais raízes de 
+-- uma equação do segundo grau.
+-- **********************************************
 DO
 $$
 DECLARE
-	realA REAL := 2.0;
-	realB REAL := 4.0;
-	realC REAL := 2.0;
+	realA REAL := 0.0;
+	realB REAL := 0.0;
+	realC REAL := 0.0;
 	realDelta REAL;
 BEGIN
-	-- realDelta := ((realB)ˆ2) - (4*(realA)*(realC));
-	-- realDelta := (( realB ˆ 2 ) - (4*( realA )*( realC )));
-	realDelta := (realB ^ 2) - 4*(realA)*(realC);
-	RAISE NOTICE 'delta % ',realDelta;	
+	SELECT (floor(random() * (99-1+1) + 1)::int) INTO realA;
+	SELECT (floor(random() * (99-1+1) + 1)::int) INTO realB;
+	SELECT (floor(random() * (99-1+1) + 1)::int) INTO realC;
+	realDelta := (realB ^ 2) - (4*(realA)*(realC));
+	RAISE NOTICE 'Delta % ',realDelta;	
 END;
 $$
 
 
+-- **********************************************
+-- 1.5 Faça um programa que gere um número 
+-- inteiro e mostre a raiz 
+-- cúbica de seu antecessor 
+-- e a raiz quadrada de seu sucessor.
+-- **********************************************
+DO 
+$$
+DECLARE
+	intRaizCubAntX INT := 0;
+	intRaizQuadSuceX INT := 0;
+	intX INT := 0;
+BEGIN
+	SELECT (random()*100::int) INTO intX;
+	-- extrai a raiz cubica de X - 1
+	intRaizCubAntX = ||/(intX - 1);
+	RAISE NOTICE ' raiz cubica do antecessor: %',intRaizCubAntX;
+	
+	-- calc raiz quadrada do sucessor
+	intRaizQuadSuceX = (intX + 1)ˆ2;
+	RAISE NOTICE ' raiz quadrada sucessor % ',intX,intRaizQuadSuceX;
+
+END;
+$$
+
+-- **********************************************
+-- 1.6 Faça um programa que gere medidas 
+-- reais de um terreno retangular. 
+-- Gere também um valor real 
+-- no intervalo [60, 70] 
+-- que representa o preço por metro quadrado. 
+-- O programa deve exibir 
+-- o valor total do terreno.
+-- **********************************************
+DO 
+$$
+DECLARE
+
+BEGIN
+
+END;
+$$
+
+-- **********************************************
+-- 1.7 Escreva um programa que gere um 
+-- inteiro que representa o ano de 
+-- nascimento de uma pessoa no intervalo 
+-- [1980, 2000] e gere um inteiro 
+-- que representa o ano atual no intervalo [2010, 2020]. 
+-- O programa deve exibir a idade da pessoa em anos. 
+-- Desconsidere detalhes envolvendo 
+-- dias, meses, anos bissextos etc.
+-- **********************************************
+DO 
+$$
+DECLARE
+
+BEGIN
+
+END;
+$$
